2be142652a89f9bab35a22fe8e7ff5413ed1cdba
Ahmed Abdeltawwab <prog.ahmed11@gmail.com>
2020-09-26 17:48:49 +0200

    Remove old version of RestApi
    Remove initial version of SdlcSystemRestController as it was created for demo and it was use repository directly
    
    Also delete related method from other classes

--- src/main/java/com/company/resourceapi/repositories/ProjectRepository.java
+++ src/main/java/com/company/resourceapi/repositories/ProjectRepository.java
@@ -1,15 +1,14 @@
 package com.company.resourceapi.repositories;
 
 import com.company.resourceapi.entities.ProjectEntity;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
-
-    Optional<ProjectEntity> findBySdlcSystemIdAndId(Long sdlcSystemId, Long projectId);
-
+    
     Optional<ProjectEntity> findBySdlcSystemIdAndExternalId(Long sdlcSystemId, String externalId);
 
     boolean existsBySdlcSystemIdAndExternalId(Long sdlcSystemId, String externalId);
